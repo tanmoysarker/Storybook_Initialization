bccad2280301b1588517f7b8168febabc23cc193
Tanmoy Sarker <25175610+tanmoysarker@users.noreply.github.com>
2022-09-09 00:08:13 +0600

    app.js updated

--- App.js
+++ App.js
@@ -27,42 +27,44 @@ import {
 import StorybookUI from './storybook';
 
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+export {default} from "./storybook";
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+// const App: () => Node = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-          <Text>Story</Text>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         style={backgroundStyle}>
+//           <Text>Story</Text>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
 
-export default App;
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+// export default App;
